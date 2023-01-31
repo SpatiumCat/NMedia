f5863678f21d43bfe73c3706ae5b1f22dcde11dd
Vladimir Baryshnikov <baryshnikovvladimirv@gmail.com>
2023-01-31 23:46:40 +0200

    Done task 2.2 MVVM

--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -1,13 +1,13 @@
 package ru.netology.nmedia
 
-class Post (
+data class Post (
     val id: Long,
     val author: String,
     val content: String,
     val published: String,
     var likedByMe: Boolean = false,
-    var likes: Int = 1499,
-    var shares: Int = 1000,
-    var views: Int = 12500
-        ) {
-}
+    val likes: Int = 1099,
+    val shares: Int = 1000,
+    val views: Int = 12500
+        )
+
