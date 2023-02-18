83f63530c2c3114839c371efceb679c5f735140e
Vladimir Baryshnikov <baryshnikovvladimirv@gmail.com>
2023-02-18 21:38:41 +0200

    Done task 3.2 Editing

---
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostActivity.kt
@@ -0,0 +1,49 @@
+package ru.netology.nmedia.activity
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.Intent.getIntent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import ru.netology.nmedia.R
+import ru.netology.nmedia.databinding.ActivityNewPostBinding
+
+class NewPostActivity : AppCompatActivity() {
+
+    val binding: ActivityNewPostBinding by lazy { ActivityNewPostBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val textResult = intent?.getStringExtra(Intent.EXTRA_TEXT)
+        if (!textResult.isNullOrBlank()) binding.contentPanel.setText(textResult)
+
+
+        binding.buttonOk.setOnClickListener {
+            val text = binding.contentPanel.text.toString()
+            if (text.isBlank()) {
+                setResult(Activity.RESULT_CANCELED)
+            } else {
+                setResult(Activity.RESULT_OK, Intent().apply { putExtra(Intent.EXTRA_TEXT, text) })
+            }
+            finish()
+        }
+
+
+    }
+
+    object NewPostContract : ActivityResultContract<String?, String?>() {
+
+        override fun createIntent(context: Context, input: String?): Intent {
+            return Intent(context, NewPostActivity::class.java).apply { putExtra(Intent.EXTRA_TEXT, input) }
+        }
+
+        override fun parseResult(resultCode: Int, intent: Intent?): String? {
+            return intent?.getStringExtra(Intent.EXTRA_TEXT)
+        }
+    }
+}
+
