2f3d32962841ef4104f66c46cba6a8772e4ef7a2
Vladimir Baryshnikov <baryshnikovvladimirv@gmail.com>
2023-04-06 20:09:51 +0200

    Task 5.1 Room (change kotlin version)

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -3,8 +3,12 @@ package ru.netology.nmedia.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.findNavController
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.messaging.FirebaseMessaging
 import ru.netology.nmedia.R
 import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
 import ru.netology.nmedia.databinding.ActivityAppBinding
@@ -40,6 +44,23 @@ class AppActivity : AppCompatActivity() {
                     textArg = text
                 }
             )
+        }
+    }
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@AppActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)){
+                getErrorDialog(this@AppActivity, code, 9000)?.show()
+                return
+            }
+            Toast.makeText(this@AppActivity, R.string.google_api_unavailable, Toast.LENGTH_LONG).show()
+        }
+
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            println(it)
         }
     }
 }
